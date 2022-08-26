e471548e78aacd8247b50efbc8ce422fa8144324
GUENFOUDI MOHAMED <mohamed.guenfoudi@hotmail.com>
2022-08-26 20:54:46 +0100

    Update btnAddAccount&closePopUpAddAccount.js

--- btnAddAccount&closePopUpAddAccount.js
+++ btnAddAccount&closePopUpAddAccount.js
@@ -10,4 +10,22 @@ const closePopUpAddAccount = document.querySelector('body .popUpAddAccount .form
 closePopUpAddAccount.onclick = function(){
     const popUpAddAccount = document.querySelector('body .popUpAddAccount');
     popUpAddAccount.classList.remove('active');
+    clearDataForm();
+}
+// function for clear data from form
+function clearDataForm(){
+    const inputs = document.querySelectorAll('body .popUpAddAccount .form > input');
+    inputs.forEach(function(value,index,arr){
+        if(index != arr.length-1){
+            inputs[index].value = '';
+        }
+    });
+    const img = document.querySelector('body .popUpAddAccount .form .uploadProfile .imgUpload .img');
+    img.src = '';
+    const selections = document.querySelectorAll('body .popUpAddAccount .form .selct');
+    selections.forEach(function(value,index,arr){
+        selections[index].selectedIndex = 0;
+    });
+    const textArea = document.querySelector('body .popUpAddAccount .form textarea');
+    textArea.value = '';
 }
